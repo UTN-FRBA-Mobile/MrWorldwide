5a5d7d9fd49cfc4361239141508eceddc039dac0
Gustrucco <gus_trucco776@yahoo.com.ar>
2018-05-01 18:58:20 -0300

    Login
    No pudimos probar. No creo que funcione pero es para tener algo sobre
    lo que trabajar

---
+++ app/src/main/java/mobile/frba/utn/tpmobile/LoginActivity.kt
@@ -0,0 +1,42 @@
+package mobile.frba.utn.tpmobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+
+class LoginActivity : AppCompatActivity() {
+    val callbackManager = CallbackManager.Factory.create()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FacebookSdk.sdkInitialize(getApplicationContext())
+        AppEventsLogger.activateApp(application)
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                        // App code
+                }
+
+                override fun onCancel() {
+                    // App code
+                }
+
+                override fun onError(exception: FacebookException) {
+                    // App code
+                }
+            })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
