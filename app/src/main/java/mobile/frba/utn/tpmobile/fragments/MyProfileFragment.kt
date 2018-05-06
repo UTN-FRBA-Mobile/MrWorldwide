9dcba8ea5b151101d460bb49fa873f7834072b69
paulucci <paulucci@strands.com>
2018-05-06 15:15:10 -0300

    Usando version con soporte de fragment

--- app/src/main/java/mobile/frba/utn/tpmobile/fragments/MyProfileFragment.kt
+++ app/src/main/java/mobile/frba/utn/tpmobile/fragments/MyProfileFragment.kt
@@ -3,16 +3,26 @@ package mobile.frba.utn.tpmobile.fragments
 import android.widget.Button
 import mobile.frba.utn.tpmobile.R
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mobile.frba.utn.tpmobile.adapters.BitacoraListAdapter
+import mobile.frba.utn.tpmobile.models.Photo
+import mobile.frba.utn.tpmobile.models.Text
 
 
-class MyProfileFragment: FragmentActivity() {
+class MyProfileFragment: Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.profile,container,false)
+    }
 
-        val btnSaveChanges = findViewById(R.id.saveChanges) as Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnSaveChanges = getView()!!.findViewById(R.id.saveChanges) as Button
         btnSaveChanges.setOnClickListener {
             //save changes
         }
