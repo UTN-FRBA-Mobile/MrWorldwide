da5f0304a6aba7a4187177b8fc76086c4bbe5bfe
paulucci <paulucci@strands.com>
2018-05-17 10:06:40 -0300

    Bug en logica de fbservice

--- app/src/main/java/mobile/frba/utn/tpmobile/services/FacebookService.kt
+++ app/src/main/java/mobile/frba/utn/tpmobile/services/FacebookService.kt
@@ -8,9 +8,9 @@ open class FacebookService {
     companion object {
         fun sessionAlive(): Boolean {
             val accessToken = AccessToken.getCurrentAccessToken()
-            val notLoggedIn = accessToken == null
+            val loggedIn = accessToken != null
             val isExpired = accessToken?.isExpired
-            return notLoggedIn  || (isExpired?:true)
+            return loggedIn  && !(isExpired?:true)
         }
     }
 }
