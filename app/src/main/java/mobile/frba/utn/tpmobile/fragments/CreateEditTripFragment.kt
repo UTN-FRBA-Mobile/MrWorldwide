6fd2b5dfc8dfe85a3a3228a1517607c2c1543819
Alejandra <alelevy15@gmail.com>
2018-05-19 12:11:54 -0300

    Seguimos con el Date Picker

--- app/src/main/java/mobile/frba/utn/tpmobile/fragments/CreateEditTripFragment.kt
+++ app/src/main/java/mobile/frba/utn/tpmobile/fragments/CreateEditTripFragment.kt
@@ -16,13 +16,6 @@ import mobile.frba.utn.tpmobile.R
 import java.text.SimpleDateFormat
 import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
-
 class CreateEditTripFragment : Fragment() {
 
     var textview_date: TextView? = null
