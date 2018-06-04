590f8b86094461229389bf7372fe44d742b96c43
feche <fecheromero@gmail.com>
2018-06-03 22:10:42 -0300

    repo trips

--- app/src/main/java/mobile/frba/utn/tpmobile/singletons/RepoTrips.kt
+++ app/src/main/java/mobile/frba/utn/tpmobile/singletons/RepoTrips.kt
@@ -19,7 +19,7 @@ object RepoTrips{
         val text = Text("Soy un titulo de relleno", DateTime.now(),"Soy un texto de relleno",LatLng(40.7,-73.9))
         val video = Video("Soy un titulo de relleno", "saraza",LatLng(41.7,-73.9))
 
-        val romaPhoto = TripPhoto("https://www.losmundosdeceli.com/wp-content/uploads/2017/05/coliseo_roma_atardecer-1080x640.jpg", DateTime.now(),LatLng(40.7,-73.9))
+        val romaPhoto = TripPhoto("https://www.losmundosdeceli.com/wp-content/uploads/2017/05/coliseo_roma_atardecer-1080x640.jpg", DateTime.now())
         val romaTrip = Trip(1,"Roma", romaPhoto, DateTime().withDate(2018, 2, 10), DateTime().withDate(2018, 2, 22), mutableListOf())
         romaTrip.events.addAll(romaTrip.events.lastIndex+1, mutableListOf(photo,text,video))
         val nyPhoto = TripPhoto("https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5244714388001_5205235439001-vs.jpg?pubId=5104226627001&videoId=5205235439001", DateTime.now())
@@ -27,7 +27,7 @@ object RepoTrips{
         val photoTk = Photo("asd", DateTime.now(), "LALALALA", LatLng(35.6,139.8))
         val textTk = Text("Soy un titulo de relleno", DateTime.now(),"Soy un texto de relleno",LatLng(36.6,139.8))
         val videoTk = Video("Soy un titulo de relleno", "saraza",LatLng(35.6,140.8))
-        val tkPhoto = TripPhoto("https://www.hola.com/imagenes/viajes/20180115104660/tokio-barrio-a-barrio-japon/0-527-396/tokio-Shibuya-t.jpg", DateTime.now(), LatLng(35.6,139.8))
+        val tkPhoto = TripPhoto("https://www.hola.com/imagenes/viajes/20180115104660/tokio-barrio-a-barrio-japon/0-527-396/tokio-Shibuya-t.jpg", DateTime.now())
         val tkTrip = Trip(3,"Tokio", tkPhoto, DateTime().withDate(2018, 2, 10), DateTime().withDate(2018, 2, 22), mutableListOf())
         tkTrip.events.addAll(tkTrip.events.lastIndex+1, mutableListOf(videoTk,textTk,photoTk))
 
