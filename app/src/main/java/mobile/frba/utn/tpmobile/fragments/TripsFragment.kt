da88f4d7a78a28de126c1d4cf8528a0e89999a73
Gustrucco <gus_trucco776@yahoo.com.ar>
2018-06-04 01:00:34 -0300

    giladas

--- app/src/main/java/mobile/frba/utn/tpmobile/fragments/TripsFragment.kt
+++ app/src/main/java/mobile/frba/utn/tpmobile/fragments/TripsFragment.kt
@@ -23,8 +23,6 @@ class TripsFragment : NavigatorFragment(null) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
         recyclerView = getView()!!.findViewById(R.id.trip_list)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         val activity = this.activity
