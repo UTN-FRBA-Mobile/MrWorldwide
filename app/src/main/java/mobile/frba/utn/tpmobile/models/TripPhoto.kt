fc310b14d7be7d9b4447baac5e7491a7b4166519
Gustrucco <gus_trucco776@yahoo.com.ar>
2018-06-25 04:41:57 -0300

    Arreglo problema de serializacion

--- app/src/main/java/mobile/frba/utn/tpmobile/models/TripPhoto.kt
+++ app/src/main/java/mobile/frba/utn/tpmobile/models/TripPhoto.kt
@@ -2,9 +2,9 @@ package mobile.frba.utn.tpmobile.models
 
 import com.google.android.gms.maps.model.LatLng
 import org.joda.time.DateTime
+import java.io.Serializable
 
 /**
  * Created by Gustavo on 5/6/18.
  */
-open class TripPhoto(var url: String, var date: DateTime)  {
-}
+open class TripPhoto(var url: String, var date: DateTime)  : Serializable { }
