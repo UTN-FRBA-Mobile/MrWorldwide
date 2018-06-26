3d093916f357c9bc7c60913cc5898507933d744d
Gustrucco <gus_trucco776@yahoo.com.ar>
2018-06-25 22:05:36 -0300

    solo un like por usuario

--- app/src/main/java/mobile/frba/utn/tpmobile/models/Video.kt
+++ app/src/main/java/mobile/frba/utn/tpmobile/models/Video.kt
@@ -1,9 +1,8 @@
 package mobile.frba.utn.tpmobile.models
 
-import com.google.android.gms.maps.model.LatLng
 import org.joda.time.DateTime
 
 /**
  * Created by Gustavo on 5/6/18.
  */
-open class Video(var description: String,mg:Int,title : String, date : DateTime, var url: String, geoLocation: Coordinate?, id : Int?, userId : String?,  tripId : Int?) : Event(EventType.VIDEO,mg, geoLocation,id, userId, tripId,date,title)
+open class Video(var description: String, likes: HashSet<String>,title : String, date : DateTime, var url: String, geoLocation: Coordinate?, id : Int?, userId : String?,  tripId : Int?) : Event(EventType.VIDEO, likes, geoLocation,id, userId, tripId,date,title)
